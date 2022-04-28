c7c6cc088aa3a6afb3847cb7d70e7211485daffd
Sacha Viquerat <sacha.vsop@gmail.com>
2022-04-28 13:59:23 +0200

    Update download_analysis_data.py

--- COMPARISON/download_analysis_data.py
+++ COMPARISON/download_analysis_data.py
@@ -3,7 +3,17 @@ import os
 import urllib.request
 from shutil import copyfile
 
+# this will result in the following structure:
+# .\COMPARISON\with IBCSO1 and SRTM\DATA
+# IBCSO_v1_bed_only_ocean.tif
+# IBCSO_v2_bed_only_ocean.tif
+# IBCSO_v2_bed_only_ocean_60deg.tif
+# SRTM15plus_V2-2_only_ocean.tif
 
+# .\COMPARISON\discrepancy sub areas\SHAPES
+# IBCSO_v1_bed_only_ocean.tif
+# IBCSO_v2_bed_only_ocean.tif
+# SRTM15plus_V2-2_only_ocean.tif
 
 filenames = {
 'IBCSO_v1': "IBCSO_v1_bed_only_ocean.tif",
@@ -13,30 +23,30 @@ filenames = {
 }
 
 links = {
-'IBCSO_v1': """https://www.dropbox.com/s/lusgh805uhz3dus/IBCSO_v1_bed_only_ocean.tif?dl=0""",
-'IBCSO_v2': """""",
-'IBCSO_v2_60': """""",
-'SRTM15': """"""
+'IBCSO_v1': """https://figshare.com/ndownloader/files/31364884?private_link=14d4c73a9f88fdff301f""",
+'IBCSO_v2': """https://figshare.com/ndownloader/files/31323850?private_link=14d4c73a9f88fdff301f""",
+'IBCSO_v2_60': """https://figshare.com/ndownloader/files/31323853?private_link=14d4c73a9f88fdff301f""",
+'SRTM15': """https://figshare.com/ndownloader/files/31323856?private_link=14d4c73a9f88fdff301f"""
 }
 
+IBCSO_SRTM_COMP_PATH = os.path.join(os.getcwd(),"with IBCSO1 and SRTM","DATA")
+IBCSO_subarea_COMP_PATH = os.path.join(os.getcwd(),"discrepancy sub areas","SHAPES")
+
+os.makedirs(IBCSO_SRTM_COMP_PATH, exist_ok=True)
+os.makedirs(IBCSO_subarea_COMP_PATH, exist_ok=True)
+
 for data, filename in filenames.items():
-    url =links[data]
-    target = os.path.join(os.getcwd(),"with IBCSO1 and SRTM","DATA",filename)
+    
+    url = links[data]
+    filename = filenames[data]
+    
+    target = os.path.join(IBCSO_SRTM_COMP_PATH,filename)
     urllib.request.urlretrieve(url, target)
+    
+    if data != 'IBCSO_v2_60':
+        target2= os.path.join(IBCSO_subarea_COMP_PATH,filename)
+        copyfile(target, target2)
+
     print(data)
     print(url)
     print(target)
-    target2= os.path.join(os.getcwd(),"discrepancy sub areas","SHAPES",filename)
-    copyfile(target, target2)
-
-    
-# into SBD2K3_MANU_SCRIPTS\COMPARISON\with IBCSO1 and SRTM\DATA
-# IBCSO_v1_bed_only_ocean.tif
-# IBCSO_v2_bed_only_ocean.tif
-# IBCSO_v2_bed_only_ocean_60deg.tif
-# SRTM15plus_V2-2_only_ocean.tif
-
-# #into SBD2K3_MANU_SCRIPTS\COMPARISON\discrepancy sub areas\SHAPES
-# IBCSO_v1_bed_only_ocean.tif
-# IBCSO_v2_bed_only_ocean.tif
-# SRTM15plus_V2-2_only_ocean.tif
